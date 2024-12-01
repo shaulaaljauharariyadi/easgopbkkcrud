e3c3cda631b706b8901b932282a6db40bfda1323
XThinct <bryantnathaniel04@gmail.com>
2024-12-01 17:39:06 +0700

    added products

--- easpbkkcrud/main.go
+++ easpbkkcrud/main.go
@@ -4,6 +4,7 @@ import (
 	"easpbkkcrud/config"
 	"easpbkkcrud/controllers/categorycontroller"
 	"easpbkkcrud/controllers/homecontroller"
+	"easpbkkcrud/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -19,6 +20,13 @@ func main() {
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	//Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("server running on port 8080")
 	http.ListenAndServe(":8080", nil)
